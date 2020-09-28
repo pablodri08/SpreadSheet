b4a222583f7f00d0ae7a223fa9c538a329f5e35f
Pablo Rodríguez <pablotomas_1@hotmail.com>
2020-09-28 04:08:55 -0300

    Ajustes en las RegExp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,9 @@ import { stringify } from 'querystring';
 const CELLPATTERN = '([A-Z]([1-9]|([1-4][0-9])|50))';
 const NUMPATTERN = '((-)?[0-9]+(\\.[0-9]*)?)';
 const ARGUMENT = '(' + NUMPATTERN + '|' + CELLPATTERN + ')';
-const SUM = 'SUM\\(' + '((' + ARGUMENT + '(,' + ARGUMENT + ')+)|(' + CELLPATTERN + ':' + CELLPATTERN + '))\\)';
-const AVG = 'AVG\\(' + '((' + ARGUMENT + '(,' + ARGUMENT + ')+)|(' + CELLPATTERN + ':' + CELLPATTERN + '))\\)';
-const BASICOP = ARGUMENT + '(\\+|\\-|\\*|/)' + ARGUMENT;
+const SUM = '^SUM\\(' + '((' + ARGUMENT + '(,' + ARGUMENT + ')+)|(' + CELLPATTERN + ':' + CELLPATTERN + '))\\)$';
+const AVG = '^AVG\\(' + '((' + ARGUMENT + '(,' + ARGUMENT + ')+)|(' + CELLPATTERN + ':' + CELLPATTERN + '))\\)$';
+const BASICOP = '^' + ARGUMENT + '(\\+|\\-|\\*|/)' + ARGUMENT + '$';
 let rowOnChange: number;
 let colOnChange: number;
 
@@ -48,6 +48,7 @@ export class AppComponent implements OnInit{
     }
   }
 
+  // Captura cambios en celdas
   initialize(): void{
     this.hot = new Handsontable(document.getElementById('hot'), {
       data: this.data,
@@ -60,6 +61,7 @@ export class AppComponent implements OnInit{
     });
   }
 
+  // Evalúa si se ingresó una fórmula
   getChanges(value): void{
     try {
       if (value){
@@ -68,15 +70,11 @@ export class AppComponent implements OnInit{
         const col = value[0][1];
         colOnChange = col;
         rowOnChange = row;
-        // Controlo si el cambio es una fórmula
         if ((input) && (input.length > 3)) {
           const first = input.charAt(0).toString();
-          let rest = input.slice(1, input.length);
-          console.log(rest);
+          const rest = input.slice(1, input.length);
           switch (first) {
             case '=':
-              console.log('Quiere operar');
-              rest = rest.replace(/ /g, '');
               const result = this.operate(rest);
               this.hot.setDataAtCell(row, col, result, '');
               break;
@@ -99,13 +97,10 @@ export class AppComponent implements OnInit{
       const regExpBasicOp = new RegExp(BASICOP);
       let result: number;
       if (regExpSum.test(value)) {
-        console.log('SUM detected\n');
         result = this.sumOperation(value.slice(4, value.length - 1));
       } else if (regExpAvg.test(value)) {
-        console.log('AVG detected\n');
         result = this.avgOperation(value.slice(4, value.length - 1));
       } else if (regExpBasicOp.test(value)) {
-        console.log('BASICOP detected\n');
         result = this.basicOperation(value);
       } else {
         throw new Error('Error en fórmula!');
@@ -138,7 +133,8 @@ export class AppComponent implements OnInit{
       let result = 0;
       let values: number[];
       values = this.getCells(value);
-      for (let i = 0; i < values.length; i++) {
+      const length = values.length;
+      for (let i = 0; i < length; i++) {
         result = result + values[i];
       }
       result = result / values.length;
@@ -148,7 +144,7 @@ export class AppComponent implements OnInit{
     }
   }
 
-  // Resuelve operación básica entre dos celdas/valores
+  // Resuelve operación básica
   basicOperation(value: string): number {
     let args: string[];
     let values: number[];
@@ -157,9 +153,6 @@ export class AppComponent implements OnInit{
       args = value.split('+');
       values = this.getValues(args);
       result = values[0] + values[1];
-      console.log('value[0]: ' + values[0]);
-      console.log('value[1]: ' + values[1]);
-      console.log('result: ' + result);
     } else if (/.*\-.*/.test(value)) {
       args = value.split('-');
       values = this.getValues(args);
@@ -180,7 +173,7 @@ export class AppComponent implements OnInit{
     return result;
   }
 
-  // Construye el rango y retorna el valor numérico de sus componentes
+  // Retorna el valor numérico de cada operando
   getCells(value: string): number[] {
     let range: string[];
     let values: number[];
@@ -213,14 +206,15 @@ export class AppComponent implements OnInit{
     const results = new Array();
     let col: number;
     let row: number;
-    for (let i = 0; i < values.length; i++) {
+    const length = values.length;
+    for (let i = 0; i < length; i++) {
       // Extraigo valor de celda si es necesario
       if (cellRegExp.test(values[i])) {
         col = values[i].charCodeAt(0);
         col = col % 'A'.charCodeAt(0);
         row = (parseInt((values[i].slice(1, values[i].length)), 10)) - 1;
         if ((row === rowOnChange) && (col === colOnChange)) {
-          throw new Error('Celda actual aún no tiene un valor!');
+          throw new Error('La celda actual no tiene un valor definido!');
         } else {
           const cellContent = this.hot.getDataAtCell(row, col);
           const stringContent = String(cellContent);
